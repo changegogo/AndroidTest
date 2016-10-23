fc270635fa20804b1433786c5f3e5dd9d36849fc
dailiwang <758017188@qq.com>
2016-10-23 15:36:31 +0800

    sencond

--- app/src/main/java/com/feicui/androidtest/androidtest/util/CommonUtil.java
+++ app/src/main/java/com/feicui/androidtest/androidtest/util/CommonUtil.java
@@ -1,11 +1,23 @@
 package com.feicui.androidtest.androidtest.util;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.view.WindowManager;
+
 import java.util.UUID;
 
 public class CommonUtil {
 
+    //获取随机uuid
 	public static String getUuid() {
         UUID uuid = UUID.randomUUID();
-        return uuid.toString();
+        return uuid.toString().replace("-","");
 	}
+    //获取屏幕的宽高
+    public static Point getScreenWidthHeight(Context context){
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Point point = new Point();
+        wm.getDefaultDisplay().getSize(point);
+        return point;
+    }
 }
--- app/src/main/java/com/feicui/androidtest/androidtest/view/LineChartView.java
+++ app/src/main/java/com/feicui/androidtest/androidtest/view/LineChartView.java
@@ -4,18 +4,26 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.feicui.androidtest.androidtest.entity.WeekData;
+import com.feicui.androidtest.androidtest.util.CommonUtil;
 
 /**
  * Author: dlw on 2016/10/18 09:54
  * Email: dailongshao@126.com
  */
 public class LineChartView extends View {
+    private final String TAG = LineChartView.class.getName();
     private Paint mPaint;
     private int[] offset = new int[7];
+    private Point mWidthHeight;
+    private int distance = 100;
+    private int mOffset;
+    private int mHeightSize;
+
 
     public LineChartView(Context context) {
         this(context,null);
@@ -34,18 +42,26 @@ public class LineChartView extends View {
         mPaint = new Paint();
         mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(2);
+        mPaint.setAntiAlias(true);
+        mPaint.setTextSize(50);
+        mWidthHeight = CommonUtil.getScreenWidthHeight(getContext());
+        mOffset = mWidthHeight.x - 7 * distance;
+
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mHeightSize = MeasureSpec.getSize(heightMeasureSpec);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         for(int i=0;i<6;i++){
-            canvas.drawLine(100+i*80,offset[i]*10,100+(i+1)*80,offset[i+1]*10,mPaint);
+            canvas.drawLine((mOffset / 2)+i*distance,mHeightSize-offset[i]*10,(mOffset / 2)+(i+1)*distance,mHeightSize-offset[i+1]*10,mPaint);
+            canvas.drawText(offset[i]+"",100+100*i,100,mPaint);
         }
+        canvas.drawText(offset[6]+"",100+100*6,100,mPaint);
     }
 
     public void setData(WeekData.DataBean dataBean){
